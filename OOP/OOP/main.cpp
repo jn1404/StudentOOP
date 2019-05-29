c5eabb8fcd58cce0942c4fff34bb9d675c6addbb
Jeffrey Na <jn1404@nyu.edu>
2019-05-29 10:34:16 -0400

    Updated emails

---
+++ OOP/OOP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOP
+//
+//  Created by Jeffrey Na on 5/29/19.
+//  Copyright © 2019 Jeffrey Na. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
