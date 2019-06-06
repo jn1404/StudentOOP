0a46a1cc24a770a1bbac1f8304e5a0ddabb2f28f
Jeffrey Na <jn1404@nyu.edu>
2019-06-06 09:35:45 -0400

    Added complex and r.txt files.

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -7,6 +7,10 @@ using namespace std;
 /*
  * A constructor for weather class.
  * */
-Weather::Weather(string nm) : station_nm(nm) {
+Weather::Weather(string nm, GPS loc) : 
+	station_nm(nm), my_loc(loc) {
 }
 
+string Weather::get_name() {
+	return station_nm;
+}
